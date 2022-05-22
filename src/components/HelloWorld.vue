7c8cdd5bc3282b56dfc7f945ae5ad80536968930
voltageDifference <98506040+voltageDifference@users.noreply.github.com>
2022-05-21 20:09:31 -0700

    currently learning tailwind :)

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header class="relative">
+    <div class="mx-8">
+      <div class="flex justify-between font-semibold mt-6">
+        <div class="text-4xl font-extrabold">
+          <span class="bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 via-orange-500 to-red-500">
+            SharkSurfer
+          </span>
+        </div>
+        <div class="flex items-center">
+          <nav>
+            <ul class="flex text-white">
+              <li>
+                <a class="btn-primary px-5 py-2" href="https://tailwindcss.com/docs/gradient-color-stops">Surf Now</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style>
+</style>
