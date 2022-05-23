f5f34c7233257e89fe9dfbb3fb01c6c7729f4906
voltageDifference <98506040+voltageDifference@users.noreply.github.com>
2022-05-23 09:29:57 -0700

    improvements in the home page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,57 +2,69 @@
 </script>
 
 <template>
-  <header class="relative">
-    <div class="mx-8">
-      <div class="flex items-center font-semibold mt-6">
-        <div class="flex basis-1/4">
-          <div class="text-4xl font-extrabold">
-            <span class="bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 via-orange-500 to-red-500">
-              SharkSurfer
-            </span>
+  <body class="antialised bg-gradient-to-r from-cyan-500 via-blue-500 to-purple-500">
+    <div class="sticky bg-white p-4">
+      <div class="mx-8">
+        <div class="flex items-center font-semibold">
+          <div class="flex basis-1/4">
+            <div class="text-4xl font-extrabold">
+              <span class="bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 via-orange-500 to-red-500">
+                SharkSurfer
+              </span>
+            </div>
           </div>
-        </div>
-        <div class="flex basis-1/2">
-          <nav class="mx-auto">
-            <ul class="flex text-white space-x-6">
-              <li>
-                <a class="btn-primary px-5 py-2" href="https://tailwindcss.com/docs/gradient-color-stops">Surf Now</a>
-              </li>
-              <li>
-                <a class="btn-primary px-5 py-2" href="https://tailwindcss.com/docs/installation">Pricing</a> 
-              </li>
-              <li>
-                <a class="btn-primary px-5 py-2" href="https://tailwindcss.com/docs/space">About Us</a> 
-              </li>
-              <li>
-                <a class="btn-primary px-5 py-2" href="https://tailwindcss.com/docs/clear">Our Stores</a> 
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div class="flex basis-1/4">
-          <nav class="ml-auto">
-            <ul class="flex space-x-3">
-              <li>
-                <a class="text-gray-500 hover:text-gray-800" href="https://tailwindcss.com/docs/content-configuration">
-                  <svg class="w-7 h-7" fill="currentColor" viewBox="0 0 16 16">
-                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-                  </svg>
-                </a>
-              </li>
-              <li>
-                <a class="text-gray-500 hover:text-gray-800" href="https://icons.getbootstrap.com/icons/facebook/">
-                  <svg class="w-7 h-7" fill="currentColor" viewBox="0 0 16 16">
-                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
-                  </svg>
-                </a>
-              </li>
-            </ul>
-          </nav>
+          <div class="flex basis-1/2">
+            <nav class="mx-auto">
+              <ul class="flex text-white space-x-6">
+                <li>
+                  <a class="btn-primary px-5 py-2" href="https://tailwindcss.com/docs/gradient-color-stops">Surf Now</a>
+                </li>
+                <li>
+                  <a class="btn-primary px-5 py-2" href="https://tailwindcss.com/docs/installation">Pricing</a> 
+                </li>
+                <li>
+                  <a class="btn-primary px-5 py-2" href="https://tailwindcss.com/docs/space">About Us</a> 
+                </li>
+                <li>
+                  <a class="btn-primary px-5 py-2" href="https://tailwindcss.com/docs/clear">Our Stores</a> 
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div class="flex basis-1/4">
+            <nav class="ml-auto">
+              <ul class="flex space-x-3">
+                <li>
+                  <a class="text-gray-500 hover:text-gray-800" href="https://tailwindcss.com/docs/content-configuration">
+                    <svg class="w-7 h-7" fill="currentColor" viewBox="0 0 16 16">
+                      <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                    </svg>
+                  </a>
+                </li>
+                <li>
+                  <a class="text-gray-500 hover:text-gray-800" href="https://icons.getbootstrap.com/icons/facebook/">
+                    <svg class="w-7 h-7" fill="currentColor" viewBox="0 0 16 16">
+                      <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                    </svg>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
     </div>
-  </header>
+    <div class="relative py-40">
+      <div class="static px-60">
+        <h1 class="text-center text-white text-7xl tracking-tight font-extrabold mb-6">
+          Navigating the web securely hasn't been easier
+        </h1>
+        <p class="text-center text-white text-3xl tracking-tight font-normal">
+          Start navigating without worries now
+        </p>
+      </div>
+    </div>
+  </body>
 </template>
 
 <style>
